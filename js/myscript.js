d9215e8c91848aa84bbf2b84c0c16097270c9fd8
PalumboAndrea <andreaseiuno.ap@gmail.com>
2022-11-24 18:00:02 +0100

    update

--- js/myscript.js
+++ js/myscript.js
@@ -6,11 +6,6 @@ Gioco dei dadi
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-Mail
-Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
-Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte?
-Se dobbiamo confrontare qualcosa che "cosa" ci serve?
-
 Consigli del giorno:
 scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
@@ -18,6 +13,8 @@ si ma noi cosa vogliamo fare?
 torniamo a scrivere in italiano
 proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 */
+
+// GIOCO DEI DADI
 
 let playerNumberElement = document.getElementById('player-number');
 let computerNumberElement = document.getElementById('computer-number');
@@ -46,5 +43,20 @@ button.addEventListener ('click', function(){
     }
 })
 
+// ESERCIZIO EMAIL
+
+/**
+CONSEGNA:
+
+Mail
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+*/
+
+let lista = ['pippo', 'pluto', 'topolino']
 
 
+
+console.log(lista[2])
