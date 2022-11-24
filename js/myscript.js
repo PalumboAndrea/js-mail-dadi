0f0b6dba61de79623ae8811445bb4656eb0cd9a0
PalumboAndrea <andreaseiuno.ap@gmail.com>
2022-11-24 15:42:07 +0100

    update

---
+++ js/myscript.js
@@ -0,0 +1,31 @@
+/**
+
+CONSEGNA:
+ 
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+Mail
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+Consigli del giorno:
+scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+si ma noi cosa vogliamo fare?
+torniamo a scrivere in italiano
+proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+*/
+
+let playerNumber = document.getElementById('player-number');
+let computerNumber = document.getElementById('computer-number');
+
+const button = document.querySelector('.btn')
+
+button.addEventListener ('click', function(){
+    playerNumber.innerHTML = Math.floor(Math.random() * 6 + 1);
+    computerNumber.innerHTML = Math.floor(Math.random() * 6 + 1); 
+})
+
